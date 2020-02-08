5b69f0d9d15124429dde8e576963b2b61acae870
Charles Harries <charlesgharries@gmail.com>
2020-02-08 15:11:52 +0000

    Styling for days

--- resources/views/components/calendar-day.blade.php
+++ resources/views/components/calendar-day.blade.php
@@ -11,4 +11,8 @@
 		<span class="text-label">{{ $day->format('D') }}</span>
 		<strong class="Day__date__number">{{ $day->format('d') }}</strong>
 	</p>
+
+	<p class="Day__link">
+		<a href="/entries/create?date={{ $day->format('Y-m-d') }}">New entry</a>
+	</p>
 </div>
