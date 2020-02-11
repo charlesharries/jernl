1725a2836f6446a6f9d6e2a0c12597bcbfc4968b
Charles Harries <charlesgharries@gmail.com>
2020-02-11 00:18:51 +0000

    Create policy for authorising users

---
+++ app/Policies/EntryPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Entry;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EntryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any entries.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the entry.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Entry  $entry
+     * @return mixed
+     */
+    public function view(User $user, Entry $entry)
+    {
+        return $entry->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can create entries.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the entry.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Entry  $entry
+     * @return mixed
+     */
+    public function update(User $user, Entry $entry)
+    {
+        return $entry->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the entry.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Entry  $entry
+     * @return mixed
+     */
+    public function delete(User $user, Entry $entry)
+    {
+        return $entry->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the entry.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Entry  $entry
+     * @return mixed
+     */
+    public function restore(User $user, Entry $entry)
+    {
+        return $entry->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the entry.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Entry  $entry
+     * @return mixed
+     */
+    public function forceDelete(User $user, Entry $entry)
+    {
+        return $entry->user_id === $user->id;
+    }
+}
