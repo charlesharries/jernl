4018df29674dbc50b84524b47ad6b0fceb7fc003
Charles Harries <charlesgharries@gmail.com>
2020-03-25 22:04:19 +0000

    Redirect home to calendar if logged-in

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,6 +8,11 @@ class PageController extends Controller
 {
     public function index()
     {
+        if (\Auth::check()) {
+            $now = (new \DateTime())->format('Y-m');
+            return redirect("/calendar/$now");
+        }
+        
         return view('index');
     }
 }
